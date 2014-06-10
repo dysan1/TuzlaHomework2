d79ebbee7e4ee8952aa67389b5e5206e50163871
SandySmajic <sysc1223@aubih.edu.ba>
2014-06-10 18:55:30 +0200

    Homework
    Tuzla Homework 2

---
+++ TuzlaHomework2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuzlaHomework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countStudent = 0;
+            int totalStudents = 6;
+            String[] studentNames = new String[6];
+            String firstName;
+            String lastName;
+
+            Console.WriteLine("Homework by Sandy Smajic");
+            while (countStudent < totalStudents)
+            {
+                Console.WriteLine("Enter Student Name: ");
+                firstName = Console.ReadLine();
+                Console.WriteLine("Enter Student Last Name: ");
+                lastName = Console.ReadLine();
+
+                studentNames[countStudent] = firstName + " " + lastName;
+                countStudent++;
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Total {0} students is entered", totalStudents);
+            Console.WriteLine("");
+
+            foreach (string s in studentNames)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("");
+            Array.Sort(studentNames);
+            Console.WriteLine("Stored Students");
+            Console.WriteLine("");
+
+            foreach (string s in studentNames)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
